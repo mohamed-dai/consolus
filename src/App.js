73a8c6001b6d4efff86dc5a9999f93425c8d0bb4
M DAI <contact.mdai@gmail.com>
2022-11-15 21:21:44 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import "./styles.css";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
+const handleKeyDown = (event) => {
+  console.log("A key was pressed: ", event.key);
+  keySwitcher(event.key);
+};
+
+const keySwitcher = (key) => {
+  switch (key) {
+    case "@":
+      showListInfos();
+      break;
+    case "?":
+      showListFields();
+      break;
+    case "=":
+      showListValues();
+      break;
+    case "Backspace":
+      doBackspace();
+      break;
+    case "Delete":
+      doDelete();
+      break;
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    case "Enter":
+      acceptItem();
+      break;
+  }
+};
+
+function App() {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <h1>Welcome to the Keydown Listening Component</h1>
     </div>
   );
 }
+
+export default App;
